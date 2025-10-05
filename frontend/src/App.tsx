b2617938cb2e834cb23b28b8764d65e04b447225
phambinh1808 <phambinh18081977@gmail.com>
2025-10-05 16:44:49 +0700

    update config project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,27 @@
 
+import { Toaster} from 'sonner';
+import { BrowserRouter,Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
+
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      {/* <button onClick={() => toast("jejejefabjsaf")}>test</button> */}
+      <BrowserRouter>
+      <Routes> 
+        <Route path="/"
+              element={<HomePage></HomePage>}/>
+        <Route path="*"
+              element={<NotFound></NotFound>}/>
+      </Routes>
+      
+      </BrowserRouter>
+      <Toaster ></Toaster>
+    </div>
+    
   )
 }
 
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HomePage = () => {
+    return <div>
+        HomPage
+    </div>;
+};
+
+export default HomePage;
